631c2fdd0cf4d10f2c71d5dfba1914cf1f76e5f1
semlevin <semenslevin@gmail.com>
2019-04-20 20:45:35 +0300

    my the first commit

---
+++ ConsoleApp2/ConsoleApp2/pch.cpp
@@ -0,0 +1,27 @@
+﻿// pch.cpp: исходный файл, соответствующий предкомпилированному заголовку; нужен для компиляции
+
+#include "pch.h"
+
+// В целом этот файл можно пропустить, но не удаляйте его, если вы используете предкомпилированные заголовки.
+int main()
+{
+	//td::cout << "Hello World!\n"; 
+	int num1, num2;
+
+	printf("Input num1: ");
+	scanf_s(" %d", &num1);
+
+	printf("\nInput num2: ");
+	scanf_s(" %d", &num2);
+	if (num1 > num2)
+	{
+		printf("num1 is bigger");
+		printf("\nnum2 oops");
+	}
+	else
+		printf("num 1 oops ");
+
+
+
+
+}
